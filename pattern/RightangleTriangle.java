03ed9949cd789c337fe03abfcc01ee293c9f9024
Manlin Roswa <manlinroswa@Manlins-MacBook-Air.local>
2024-07-19 19:03:12 +0530

    first commit

---
+++ pattern/RightangleTriangle.java
@@ -0,0 +1,14 @@
+public class RightangleTriangle {
+    public static void main(String[] args) {
+        int n=1;
+        for(int row=1; row<=5; row++){
+            for(int col=1; col<=row; col++){
+               
+                System.out.print(n+" "); 
+                n++;
+            }
+            System.out.println();
+        }
+    }
+    
+}
