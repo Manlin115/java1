be5c3b34b42c2c5e6a25fe7a2100f26c948e05c3
Manlin Roswa <manlinroswa@Manlins-MacBook-Air.local>
2024-07-21 21:06:17 +0530

    java

---
+++ Array/Array.java
@@ -0,0 +1,16 @@
+public class Array{
+    public static void main(String[] args)
+    {
+    int[] marks={90,80,78,99,55,32};
+    System.out.println(marks.length);
+    for(int i=0;i<marks.length;i++)
+    System.out.println(marks[i]);
+    System.out.println();
+    
+    char[] name={'M','A','N','L','I','N'};
+    System.out.println(name.length);
+    for(int i=0;i<name.length;i++)
+    System.out.println(name[i]);
+    }
+    
+    }
---
+++ Array/SimpleArray.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class SimpleArray{
+public static void main(String[] args)
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter Count");
+int no_of_sub=sc.nextInt();
+int [] marks=new int[no_of_sub];
+
+int total = 0;
+    for(int i = 0; i<marks.length; i++)
+      {
+      System.out.println("Enter Mark: ");
+      marks[i] = sc.nextInt();
+      //total = total + marks[i];
+      total+=marks[i];
+      }
+    System.out.println("Total mark is: " + total);
+    System.out.println("Percentage is: " + total/marks.length);
+    if(total>450)
+      System.out.println("A Grade");
+    else if(total>350)
+      System.out.println("B Grade");
+    else
+      System.out.println("c Grade");
+  
+     
+}
+
+}
