431e282b16df39cce129e5bede6e9dba48189ad7
Manlin Roswa <manlinroswa@Manlins-MacBook-Air.local>
2024-07-31 21:38:20 +0530

    First Commit

---
+++ Packages/Subdealer.java
@@ -0,0 +1,10 @@
+package tamilnadu.madurai.sellur;
+
+public class Subdealer {
+    public void delever(int amount){
+        
+        System.out.println("Delever amonut is:"+amount);
+
+    }
+    
+}
---
+++ Packages/SuperMarket.java
@@ -0,0 +1,13 @@
+package tamilnadu.madurai.arappalayam;
+import tamilnadu.madurai.sellur.Subdealer;
+
+public class SuperMarket
+{
+    public static void main(String[] args) {
+        Subdealer sealsrep= new Subdealer();
+        sealsrep.delever(1000);
+        // System.out.println("hello Madurai");
+        
+    }
+
+}
